07b12b57446c0e4d1af21d7def1a1a76074e7eba
makeda <adm670in@gmail.com>
2026-04-29 19:14:15 +0000

    modifierInscription

--- js/app.js
+++ js/app.js
@@ -4,11 +4,15 @@ import {
     formAjout,
     btnFermerAjout,
     zoneToast,
+    listeInscription,
+    
  } from "./dom/element.js"
 // import { btnAjouter } from "./dom/element.js"
 import { getData ,sauvegardeData} from "./store/taskStore.js"
-import { verifData,resetErrors,afficherErreurs,saisiInscription,ajoutInscription,afficheOneIns,afficheAllIns } from "./services/taskService.js"
+import { verifMail,verifTelephone,verifData,resetErrors,afficherErreurs,saisiInscription,ajoutInscription,afficheOneIns,afficheAllIns,rechargerFormulaire, modifUser } from "./services/taskService.js"
 import { afficherToast } from "./ui/messageRenderer.js"
+
+let idModifier = null
 
  btnAjouter.addEventListener("click",function(){
     modalAjout.classList.remove('hidden')
@@ -17,6 +21,17 @@ import { afficherToast } from "./ui/messageRenderer.js"
  btnFermerAjout.addEventListener("click",function(){
     modalAjout.classList.add("hidden")
  })
+
+listeInscription.addEventListener("click",function(event){
+  const btnModif = event.target.closest("#btnModifier")
+  console.log(btnModif)
+   if(btnModif){
+      idModifier = parseInt(btnModif.dataset.id)
+      modalAjout.classList.remove('hidden')
+      rechargerFormulaire(idModifier)
+   }
+})
+
  formAjout.addEventListener("submit",function(event){
    event.preventDefault()
    let nomValue = nom.value.trim()
@@ -25,16 +40,27 @@ import { afficherToast } from "./ui/messageRenderer.js"
    let telephoneValue = telephone.value.trim()
    let selectFormationValue = selectFormation.value.trim()
 
-   let errors = verifData(nomValue,prenomValue,emailValue,telephoneValue,selectFormationValue)
+   let errors = verifData(nomValue,prenomValue,emailValue,telephoneValue,selectFormationValue,idModifier)
     if(Object.keys(errors).length > 0){
         afficherErreurs(errors)
         return
     }
-    const ajoutInscrip = saisiInscription(nomValue,prenomValue,emailValue,telephoneValue,selectFormationValue)
-    ajoutInscription(ajoutInscrip)
-    modalAjout.classList.add('hidden')
-   afficherToast('Inscription ajouté avec succcés','succes')
-   formAjout.reset()
-   
+    if(idModifier === null){
+       const ajoutInscrip = saisiInscription(nomValue,prenomValue,emailValue,telephoneValue,selectFormationValue)
+       ajoutInscription(ajoutInscrip)
+      
+      afficherToast('Inscription ajouté avec succcés','succes')
+      
+    }else{
+         modifUser(idModifier,prenomValue,nomValue,emailValue,telephoneValue,selectFormationValue)
+         
+         afficherToast('Modification ajouté avec succcés','succes')
+         idModifier = null
+      }
+    
+      listeInscription.innerHTML = ""  // ← vider
+      afficheAllIns()                  // ← reremplir
+      formAjout.reset()
+      modalAjout.classList.add('hidden')
 })
-afficheAllIns()
+ afficheAllIns()  
